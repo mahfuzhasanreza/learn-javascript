2e53073ad3dfb123ea74421d78c25c33858d5ff8
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-09-12 11:36:29 +0600

    added var types

---
+++ var_types.js
@@ -0,0 +1,10 @@
+var name = "Mahfuz Hasan"; // can be ''
+console.log(name);
+
+// boolean
+var isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+// number
+var age = 17;
