f3464fd2ceb8c7517a6204baf77062d32947f6d2
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-03 20:07:08 +0600

    Template String, Multiple Line String, Dynamic String

---
+++ es6/template.js
@@ -0,0 +1,11 @@
+
+const a = 10;
+const b = 20;
+// const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+const result = `The sum of ${a} ans ${b} is ${a + b}`
+console.log(result);
+
+// const name = 'Mahfuz \nHasan';
+const name = `Mahfuz
+Hasan`
+console.log(name);
