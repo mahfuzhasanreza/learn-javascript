e7c758e7754485e39e1af0fe4ab169a3f60c8f35
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-03 20:26:20 +0600

    added Big Arrow Function

--- es6/arrow.js
+++ es6/arrow.js
@@ -14,4 +14,13 @@ const add2 = function (a, b) {
 // arrow function 
 const printHello = () => console.log('Hello');
 const printA = a => console.log(a);
-const add3 = (a, b) => a + b;
+const add3 = (a, b) => a + b; // implicit return
+
+printHello();
+console.log(add3(2, 3));
+
+// large arrow function
+const getSum = (x, y) => {
+    const sum = x + y;
+    return sum;
+}
