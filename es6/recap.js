e94d56febb161205f2f9b528c7c3c0368b1cde40
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-03 23:20:13 +0600

    Basic ES6 Recap

---
+++ es6/recap.js
@@ -0,0 +1,10 @@
+/* 
+ * 1. var let const [let, const: block scope]
+ * 2. default parameter
+ * 3. template string [`${}`]
+ * 4. arrow function
+ * 5. destructuring ans spread operator
+ * 6. Object.keys [not es6], Object.values [not es6], Object.entries
+ * 7. for of [used in array and string]
+ * 8. for in [used in object]
+*/
