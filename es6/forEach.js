98769a264725533b714123ee328ba33481805a78
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-04 08:59:22 +0600

    modify foreach file

--- es6/forEach.js
+++ es6/forEach.js
@@ -1,10 +1,10 @@
-const numbers = [1,3,2,4];
+const numbers = [1, 3, 2, 4];
 
 
-const result = numbers.forEach(n=>console.log(n));
-const result2 = numbers.forEach(n=>{
+const result = numbers.forEach(n => console.log(n));
+const result2 = numbers.forEach(n => {
     console.log(n);
-    return n*n;
+    return n * n;
 });
 
 console.log(result, result2);
