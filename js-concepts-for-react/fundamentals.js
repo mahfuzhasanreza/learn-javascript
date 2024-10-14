921be91139161b95a01b96936622030f547a1a55
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-15 00:54:51 +0600

    added Six JavaScript Fundamentals

---
+++ js-concepts-for-react/fundamentals.js
@@ -0,0 +1,28 @@
+// 1. declare a variable
+const myName = 'Mahfuz';
+let season = 'summer';
+season = 'winter';
+
+// 2. conditions
+// 6 basic condition: >, <, ===, !==, <=, >=
+// multiple conditions: &&, ||
+// if, else-if
+
+// 3. array
+// index, length, push 
+const numbers = [];
+
+// 4. loop
+// for loop
+
+// 5. function
+function add() { }
+
+// 6. object 
+// 2 ways to access property by name 
+const student = {
+    name: 'mahfuz',
+    age: 17
+}
+console.log(student.age); // direct by property
+console.log(student['age']); // access via property name string
